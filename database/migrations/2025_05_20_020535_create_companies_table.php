f9baf23c3d5234936e46cf5deba53cb58b31615e
Gabriela Muñoz Nupan <gabriela.munoz@evertecinc.com>
2025-05-21 22:12:53 -0500

    fix: add index to company name field for improved query performance

--- database/migrations/2025_05_20_020535_create_companies_table.php
+++ database/migrations/2025_05_20_020535_create_companies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('nit', length: 9)->unique();
-            $table->string('name', length: 100);
+            $table->string('name', length: 100)->index();
             $table->string('address', length: 100);
             $table->string('phone_number', length: 10);
             $table->enum('status', CompanyStatus::getStatusList())->default(CompanyStatus::ACTIVE);
